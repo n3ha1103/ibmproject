0a443e7542d676cc5dab95067961a8c5b5df681a
neha1103 <153169237+n3ha1103@users.noreply.github.com>
2025-05-30 12:30:07 +0530

    Add files via upload

---
+++ test_app.py
@@ -0,0 +1,13 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get("/")
+    assert response.data == b"Hello, DevOps World!"
+    assert response.status_code == 200
+
